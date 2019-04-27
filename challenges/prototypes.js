14600b60a7637422a7678f43211eb3c09807b2cf
Allie Robinson <allierob93@gmail.com>
2019-04-26 18:37:09 -0700

    MVP requirements met.

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -6,19 +6,25 @@
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
 
-class CuboidMaker {
-  constructor(attributes) {
+// class CuboidMaker {
+//   constructor(attributes) {
+//   this.length = attributes.length;
+//   this.width = attributes.width;
+//   this.height = attributes.height;
+//   }
+//   volume() {
+//     return this.length * this.width * this.height;
+//   }
+//   surfaceArea() {
+//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+//   }
+// }
+
+function CuboidMaker(attributes) {
   this.length = attributes.length;
   this.width = attributes.width;
   this.height = attributes.height;
-  }
-  volume() {
-    return this.length * this.width * this.height;
-  }
-  surfaceArea() {
-    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-  }
-}
+};
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
@@ -26,6 +32,9 @@ class CuboidMaker {
   Formula for cuboid volume: length * width * height
 */
 
+CuboidMaker.prototype.volume = function () {
+  return this.length * this.width * this.height;
+};
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
@@ -33,6 +42,9 @@ class CuboidMaker {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
+CuboidMaker.prototype.surfaceArea = function () {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+};
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
